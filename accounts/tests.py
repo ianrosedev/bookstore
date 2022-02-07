a5f1713c591d2805e4dec8dd819f6731394f03fd
Ian Rose <ianrosedev@gmail.com>
2022-02-07 11:44:15 -0500

    Custom user model setup

---
+++ accounts/tests.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class CustomUserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username="test",
+            email="test@example.com",
+            password="testpass123",
+        )
+
+        self.assertEqual(user.username, "test")
+        self.assertEqual(user.email, "test@example.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_super_user(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username="superadmin",
+            email="superadmin@example.com",
+            password="testpass123",
+        )
+
+        self.assertEqual(admin_user.username, "superadmin")
+        self.assertEqual(admin_user.email, "superadmin@example.com")
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,7 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Custom user model
+
+AUTH_USER_MODEL = "accounts.CustomUser"
